bc71fd7686b9c69349f1974fc104c7cf621c6056
Piotrek <@>
2017-10-25 23:13:46 +0200

    przeslanie plików

---
+++ A_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_10
+{
+    class Program
+    {
+        static string zamienZnaki(string napis)
+        {
+            if (napis.Length > 1)
+            {
+                return napis = napis.Substring(napis.Length - 1) + napis.Substring(1, napis.Length - 2) + napis.Substring(0, 1);
+            }
+            else return napis;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź ciąg znaków i naciśnij Enter.");
+            string abc = Console.ReadLine();
+            Console.WriteLine(zamienZnaki(abc));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+Napisz programy, który pobiera z klawiatury ciąg znaków. Następnie zamienia w nim pierwszy i ostatni znak. Wynik wyświetlony jest na konsoli.
+
+Przykładowe wejście:
+
+sadffg444rf
+
+Przykładowe wyjście:
+
+fadffg444rs
